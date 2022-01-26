e0ac3533d61d999ab0f0788f9f4b7e10e0731820
17010-75-10 <17010-75-10@exchange.ad.afpanet>
2022-01-26 16:54:21 +0100

    26-01-2022

--- essais codage/animation css js/formulaire-kevin/js/form.js
+++ essais codage/animation css js/formulaire-kevin/js/form.js
@@ -2,11 +2,11 @@ let form = document.querySelector('form'); // il lui donne un nom puis choppe la
 let ecoute_Nom = document.querySelector('.nom'); // il lui donne un nom puis choppe la classe
 let ecoute_Prenom = document.querySelector('.prenom');// il lui donne un nom puis choppe la classe
 let ecoute_Date = document.querySelector('.date');// il lui donne un nom puis choppe la classe
-let ecoute_Mail = document.querySelector('.mail');// il lui donne un nom puis choppe la classe
+let ecoute_Mail = document.querySelector('.mail');// il lui donne un nom puis choppe la classe     //on recup tous les inputs form et button
 let ecoute_Pwd = document.querySelector('.pwd');// il lui donne un nom puis choppe la classe
 let ecoute_Cpwd = document.querySelector('.cpwd');// il lui donne un nom puis choppe la classe
 let submit = document.querySelector('button');// il lui donne un nom puis choppe la classe
-let compteur = 0; // +1 pour chaque ligne d'infos valide
+let compteur = 0; // +1 pour chaque ligne d'infos valide sinon 0
 
 submit.addEventListener('click',function(){ // fonction du bouton submit on écoute ce que ça produit
 
@@ -29,19 +29,19 @@ submit.addEventListener('click',function(){ // fonction du bouton submit on éco
         compteur = compteur + 1
     }else{
         ecoute_Date.insertAdjacentHTML("beforebegin", '<p>et avec une date?</p>');
-    }
-    if(ecoute_Mail.value.indexOf('@') == -1){
-        ecoute_Mail.insertAdjacentHTML("beforebegin", '<p> essaies avec une vraie adresse mail pour voir!</p>');
-    }else{
+    } // importe du texte qui n'existe pas dans le code en message d'erreur
+    if(ecoute_Mail.value.indexOf('@ && .') == -1){ // vérifie la condition de présence de l'arobase
+        ecoute_Mail.insertAdjacentHTML("beforebegin", '<p> essaies avec une vraie adresse mail pour voir!</p>'); // importe du texte qui n'existe pas dans le code en message d'erreur
+    }else{ 
         compteur = compteur + 1;
     }
-    if(ecoute_Pwd.value != ecoute_Cpwd.value || ecoute_Pwd < 8){
+    if(ecoute_Pwd.value != ecoute_Cpwd.value || ecoute_Pwd < 8){ // on compare le password avec la confirmation avec un "ou"
         ecoute_Pwd.insertAdjacentHTML("beforebegin", '<p> Mdp trop petit comme ton cerveau ou different de ta confirmation de Mdp et la je peux rien pour toi...</p>');
-    }else{
+    }else{ // importe du texte qui n'existe pas dans le code en message d'erreur
         compteur = compteur + 1;
     }
 
-    if(compteur==5){
+    if(compteur==5){ // si toutes les infos sont valides alors le compteur sera a 5 validera toutes les infos avec une alerte et les valeurs des inputs seront reset
         ecoute_Nom.value="";
         ecoute_Prenom.value="";
         ecoute_Date.value="";
@@ -50,7 +50,7 @@ submit.addEventListener('click',function(){ // fonction du bouton submit on éco
         ecoute_Cpwd.value="";
         alert("c'est good! les infos sont sur le serveur")
     }else{
-        compteur = 0;
+        compteur = 0; // si un ou plusieurs inputs sont pas bons le compteur est reset.
     }
 
 })
